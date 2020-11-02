a51c09eba5e59ad8c51ed28408724400a3bd5273
xyamix <xyamix1337@gmail.com>
2020-11-02 16:47:14 +0300

    homework 1

--- homework 1/task3.py
+++
@@ -1,3 +0,0 @@
-n = int(input("Enter n: "))
-s = n + 11*n + 111*n
-print(s)
--- homework 1/task4.py
+++
@@ -1,8 +0,0 @@
-n = input("Enter n: ")
-i = 0
-max = 0
-while(i != len(n)):
-    if int(n[i]) > max:
-        max = int(n[i])
-    i += 1
-print(max)
